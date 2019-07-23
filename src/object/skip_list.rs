e7af0bd33d3437d9fc3a216c551ac14f5a3f2a86
huangjiahua <hjh4477@outlook.com>
2019-07-23 22:50:44 +0800

    finish insert

--- src/object/skip_list.rs
+++ src/object/skip_list.rs
@@ -2,7 +2,7 @@ use std::rc::Rc;
 use crate::object::{RobjPtr, Robj, RobjType, Sds};
 use rand::prelude::*;
 use std::iter::Skip;
-use core::borrow::Borrow;
+use core::borrow::{Borrow, BorrowMut};
 use std::cell::{Ref, RefCell};
 
 const SKIP_LIST_MAX_LEVEL: usize = 32;
@@ -15,7 +15,7 @@ pub struct SkipListLevel {
 pub struct SkipListNode {
     obj: Option<RobjPtr>,
     score: f64,
-    backward: Option<Rc<SkipListNode>>,
+    backward: Option<Rc<RefCell<SkipListNode>>>,
     level: Vec<SkipListLevel>,
 }
 
@@ -98,7 +98,7 @@ impl SkipList {
 
         let mut rank = [0usize; SKIP_LIST_MAX_LEVEL];
 
-        let mut x: Option<Rc<RefCell<SkipListNode>>> = Some(Rc::clone(&self.header));
+        let mut x = Rc::clone(&self.header);
 
         for i in (0..self.level).rev() {
             rank[i] = if i == self.level - 1 {
@@ -107,19 +107,89 @@ impl SkipList {
                 rank[i + 1]
             };
 
-            let mut x_ref = x.as_ref();
-            let mut node_ref = x_ref.unwrap().as_ref().borrow();
-            let mut forward = node_ref.level[i].forward.as_ref();
 
-            while forward.is_some() &&
-                (forward.unwrap().as_ref().borrow().score < score ||
-                    (forward.unwrap().as_ref().borrow().score == score &&
-                        forward.unwrap().as_ref().borrow().obj.as_ref()
-                            .unwrap().as_ref().borrow().string()
-                            < obj.as_ref().borrow().string())) {
-                rank[i] += x.as_ref().unwrap().as_ref().borrow().level[i].span;
+            loop {
+                let curr = Rc::clone(&x);
+                let this_node = curr.as_ref().borrow();
+                if this_node.level[i].forward.is_none() {
+                    break;
+                }
+                let forward = this_node.level[i]
+                    .forward.as_ref().unwrap();
+                let next_node = forward.as_ref().borrow();
+                let next_score = next_node.score.clone();
+                let next_obj = next_node.obj.as_ref().unwrap().as_ref().borrow();
+
+                if next_score > score || (next_score == score &&
+                    next_obj.string() >= obj.as_ref().borrow().string()) {
+                    break;
+                }
+
+                rank[i] += this_node.level[i].span;
+
+                x = Rc::clone(forward);
             }
+
+            update[i] = Some(Rc::clone(&x));
         }
+
+        let level = SkipList::random_level();
+
+        if level > self.level {
+            for i in self.level..level {
+                rank[i] = 0;
+                update[i] = Some(Rc::clone(&self.header));
+                update[i].as_ref().unwrap()
+                    .as_ref().borrow_mut().level[i].span = self.length;
+            }
+
+            self.level = level;
+        }
+
+        let new_node = Rc::new(
+            RefCell::new(
+                SkipListNode::new(level, score, Some(obj))
+            )
+        );
+        let curr = new_node.as_ref();
+
+        for i in 0..level {
+            let prev = update[i].as_ref().unwrap().as_ref();
+
+            curr.borrow_mut().level[i].forward = match prev.borrow().level[i].forward {
+                None => None,
+                Some(_) => Some(Rc::clone(prev.borrow().level[i]
+                    .forward.as_ref().unwrap())),
+            };
+
+            prev.borrow_mut().level[i].forward = Some(Rc::clone(&new_node));
+
+            curr.borrow_mut().level[i].span
+                = prev.borrow().level[i].span - (rank[0] - rank[i]);
+
+            prev.borrow_mut().level[i].span = (rank[0] - rank[i]) + 1;
+        }
+
+        for i in level..self.level {
+            update[i].as_ref().unwrap().as_ref().borrow_mut().level[i].span += 1;
+        }
+
+        curr.borrow_mut().backward = if Rc::ptr_eq(
+            &self.header, update[0].as_ref().unwrap(),
+        ) {
+            None
+        } else {
+            Some(Rc::clone(update[0].as_ref().unwrap()))
+        };
+
+        if let Some(e) = curr.borrow().level[0].forward.as_ref() {
+            e.as_ref().borrow_mut().backward = Some(Rc::clone(&new_node));
+        } else {
+            self.tail = Some(Rc::clone(&new_node));
+        }
+
+        self.length += 1;
+
     }
 }
 
