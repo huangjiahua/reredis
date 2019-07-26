e55bd1d2e6e3c8920c472245ed487580d017a0f5
huangjiahua <hjh4477@outlook.com>
2019-07-26 14:01:10 +0800

    update int_set

--- src/object/int_set.rs
+++ src/object/int_set.rs
@@ -15,6 +15,77 @@ impl IntSet {
         IntSet(int_set)
     }
 
+    pub fn add(&mut self, value: i64) -> Result<(), ()> {
+        let val_enc = Self::value_encoding(value);
+        if val_enc > self.encoding() {
+            return self.upgrade_and_add(value);
+        }
+
+        let pos = self.search(value);
+        if let None = pos {
+            return Err(());
+        }
+
+        let pos = pos.unwrap();
+        let old_len = self.len();
+
+        self.resize(self.len() + 1);
+
+        if pos < old_len {
+            self.move_tail(pos, pos + 1);
+        }
+
+        self.set(pos, value);
+
+        Ok(())
+    }
+
+    fn upgrade_and_add(&mut self, value: i64) -> Result<(), ()> {
+        unimplemented!()
+    }
+
+    fn resize(&mut self, len: usize) {}
+
+    fn move_tail(&mut self, from: usize, to: usize) {
+        assert!(to > from);
+        let from = self.true_pos(from);
+        let to = self.true_pos(to);
+        let diff = to - from;
+        self.0.resize(diff + self.0.len(), 0);
+
+        for p in (to..self.0.len()).rev().map(|x| (x, x - diff)) {
+            self.0.swap(p.0, p.1);
+        }
+    }
+
+    fn set(&mut self, pos: usize, value: i64) {
+        let pos = self.true_pos(pos);
+
+        for i in 0..(self.encoding() as usize) / mem::size_of::<i8>() {
+            let mut v = value >> (i * mem::size_of::<i8>()) as i64;
+            v &= 0xff;
+            assert!(v < std::i8::MAX as i64);
+            self.0[pos + i] = v as i8;
+        }
+    }
+
+    fn get(&self, pos: usize) -> i64 {
+        let pos = self.true_pos(pos);
+        let mut v = 0i64;
+
+        for i in (0..(self.encoding() as usize) / mem::size_of::<i8>()).rev() {
+            v <<= mem::size_of::<i8>() as i64;
+            v |= self.0[pos + i] as i64;
+        }
+
+        v
+    }
+
+    pub fn search(&self, value: i64) -> Option<usize> {
+        unimplemented!()
+    }
+
+
     pub fn len(&self) -> usize {
         let enc = self.0[0] as usize;
         (self.0.len() - 4) / enc
@@ -32,6 +103,10 @@ impl IntSet {
             return INT_SET_ENC_INT32;
         }
         INT_SET_ENC_INT16
+    }
+
+    fn true_pos(&self, pos: usize) -> usize {
+        pos * (self.encoding() as usize) + 4
     }
 }
 
@@ -54,5 +129,33 @@ mod test {
         assert_eq!(IntSet::value_encoding((i16::MAX as i64) + 1), INT_SET_ENC_INT32);
         assert_eq!(IntSet::value_encoding(i32::MAX as i64), INT_SET_ENC_INT32);
         assert_eq!(IntSet::value_encoding((i32::MAX as i64) + 1), INT_SET_ENC_INT64);
+    }
+
+    #[test]
+    fn move_tail_test() {
+        let mut set = IntSet::new();
+        set.move_tail(0, 4);
+        assert_eq!(set.len(), 4);
+    }
+
+    #[test]
+    fn simple_add() {
+        let mut set = IntSet::new();
+        set.add(1);
+        set.add(2);
+    }
+
+    fn simple_set_get(v: i64) {
+        let mut set = IntSet::new();
+        set.move_tail(0, 1);
+        set.set(0, 1);
+        assert_eq!(set.get(0), 1);
+    }
+
+    #[test]
+    fn set_and_get() {
+        for i in 0..i16::MAX {
+            simple_set_get(i);
+        }
     }
 }
