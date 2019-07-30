5bfc1db5e7d134342810251edce94db15f39be68
huangjiahua <hjh4477@outlook.com>
2019-07-30 14:37:26 +0800

    finish encoding parsing

--- src/object/zip_list.rs
+++ src/object/zip_list.rs
@@ -13,10 +13,17 @@ enum Encoding {
 }
 
 impl Encoding {
-    fn size(&self) -> usize {
+    fn unwrap_str(&self) -> usize {
         match self {
             Encoding::Str(sz) => *sz,
-            Encoding::Int(sz) => 1,
+            _ => panic!("this is an int encoding"),
+        }
+    }
+
+    fn unwrap_int(&self) -> i64 {
+        match self {
+            Encoding::Int(v) => *v,
+            _ => panic!("this is a str encoding"),
         }
     }
 
@@ -43,8 +50,23 @@ impl Encoding {
                 assert!(*sz < 1 << 32);
                 5
             }
-            Encoding::Int(sz) => {
-                1
+            Encoding::Int(v) => {
+                if *v > 0 && *v < 12 {
+                    return 1;
+                }
+                if *v > std::i8::MIN as i64 && *v < std::i8::MAX as i64 {
+                    return 1 + mem::size_of::<i8>();
+                }
+                if *v > std::i16::MIN as i64 && *v < std::i16::MAX as i64 {
+                    return 1 + mem::size_of::<i16>();
+                }
+                if *v > -(1 << 23) && *v < (1 << 23 - 1) {
+                    return 1 + 3;
+                }
+                if *v > std::i32::MIN as i64 && *v < std::i32::MAX as i64 {
+                    return 1 + mem::size_of::<i32>();
+                }
+                1 + mem::size_of::<i64>()
             }
         }
     }
@@ -69,28 +91,32 @@ impl Encoding {
                 _ => {}
             }
         }
-        if len == 5 {
-            idx -= 1;
-        }
-        v |= ((self.size() >> (idx * 8)) & 0xff);
+        v |= ((self.unwrap_str() >> ((len - idx - 1) * 8)) & 0xff);
         v as u8
     }
 
     fn index_int(&self, idx: usize) -> u8 {
+        assert!(idx < self.blob_len());
         if let Encoding::Int(v) = self {
-            if *v > 0 && *v < 12 {
-                return *v as u8 | 0b1111_0000;
+            if idx == 0 {
+                if *v > 0 && *v < 12 {
+                    return *v as u8 | 0b1111_0000;
+                }
+                if *v > std::i8::MIN as i64 && *v < std::i8::MAX as i64 {
+                    return ZIP_LIST_I8_ENC;
+                }
+                if *v > std::i16::MIN as i64 && *v < std::i16::MAX as i64 {
+                    return ZIP_LIST_I16_ENC;
+                }
+                if *v > -(1 << 23) && *v < (1 << 23 - 1) {
+                    return ZIP_LIST_I24_ENC;
+                }
+                if *v > std::i32::MIN as i64 && *v < std::i32::MAX as i64 {
+                    return ZIP_LIST_I32_ENC;
+                }
+                return ZIP_LIST_I64_ENC;
             }
-            if *v > std::i16::MIN as i64 && *v < std::i16::MAX as i64 {
-                return ZIP_LIST_I16_ENC;
-            }
-            if *v > -(1 << 23) && *v < (1 << 23 - 1) {
-                return ZIP_LIST_I24_ENC;
-            }
-            if *v > std::i32::MIN as i64 && *v < std::i32::MAX as i64 {
-                return ZIP_LIST_I32_ENC;
-            }
-            return ZIP_LIST_I64_ENC;
+            return ((*v >> (self.blob_len() - idx - 1) as i64 * 8) & 0xff) as u8;
         }
         panic!("This is not a str encoding")
     }
@@ -100,6 +126,64 @@ impl Encoding {
             enc: self.clone(),
             curr: 0,
         }
+    }
+
+    fn is_str_enc(x: &[u8]) -> bool {
+        x[0] & 0b1100_0000 != 0b1100_0000
+    }
+
+    fn is_int_enc(x: &[u8]) -> bool {
+        !Self::is_str_enc(x)
+    }
+
+    fn parse(x: &[u8]) -> Encoding {
+        match Self::is_str_enc(x) {
+            true => Self::parse_str_enc(x),
+            false => Self::parse_int_enc(x),
+        }
+    }
+
+    fn parse_str_enc(x: &[u8]) -> Encoding {
+        let sz = match x[0] & 0b1100_0000 {
+            0b0000_0000 => 1usize,
+            0b0100_0000 => 2usize,
+            0b1000_0000 => 5usize,
+            _ => panic!("not possible"),
+        };
+        let mut v = x[0] as usize & 0b0011_1111;
+        for i in 1..sz {
+            v <<= 8;
+            v |= x[i] as usize;
+        }
+        Encoding::Str(v)
+    }
+
+    fn parse_int_enc(x: &[u8]) -> Encoding {
+        let sz = match x[0] {
+            ZIP_LIST_I16_ENC => mem::size_of::<i16>(),
+            ZIP_LIST_I32_ENC => mem::size_of::<i32>(),
+            ZIP_LIST_I64_ENC => mem::size_of::<i64>(),
+            ZIP_LIST_I24_ENC => 3,
+            ZIP_LIST_I8_ENC => mem::size_of::<i8>(),
+            _ => {
+                if x[0] >> 4 != 0b1111 {
+                    panic!("not int encoding");
+                }
+                let k = x[0] & 0x0f;
+                assert!(k > 0 && k < 12);
+                return Encoding::Int(k as i64);
+            }
+        };
+        let mut v = if x[1] >> 7 == 1 {
+            -1i64
+        } else {
+            0i64
+        };
+        for i in 0..sz {
+            v <<= 8;
+            v |= x[i + 1] as i64;
+        }
+        Encoding::Int(v)
     }
 }
 
@@ -196,7 +280,9 @@ impl ZipList {
         self.get_usize_value(0, ZIP_LIST_TAIL_OFF_SIZE)
     }
 
-    fn insert(&mut self, off: usize, s: &[u8]) {}
+    fn insert(&mut self, off: usize, s: &[u8]) {
+        unimplemented!()
+    }
 }
 
 
@@ -220,6 +306,9 @@ mod test {
         }
 
         let enc = Encoding::Int(0);
+        assert_eq!(enc.index(0), ZIP_LIST_I8_ENC);
+
+        let enc = Encoding::Int(128);
         assert_eq!(enc.index(0), ZIP_LIST_I16_ENC);
 
         let enc = Encoding::Int(32768);
@@ -230,5 +319,71 @@ mod test {
 
         let enc = Encoding::Int(1 << 31);
         assert_eq!(enc.index(0), ZIP_LIST_I64_ENC);
+    }
+
+    #[test]
+    fn str_encoding() {
+        for i in 0..64 {
+            let enc = Encoding::Str(i);
+            assert_eq!(enc.iter().next().unwrap(), i as u8);
+        }
+
+        for i in 64..16383 {
+            let enc = Encoding::Str(i);
+            let mut arr = [0, 0];
+            arr[1] = (i & 0xff) as u8;
+            arr[0] = (i >> 8) as u8 | 0b0100_0000;
+            for p in enc.iter().zip(arr.iter()) {
+                assert_eq!(p.0, *p.1);
+            }
+        }
+
+        let enc = Encoding::Str(0xffff_ffff);
+        let arr = [0x80u8, 0xff, 0xff, 0xff, 0xff];
+        for p in enc.iter().zip(arr.iter()) {
+            assert_eq!(p.0, *p.1);
+        }
+    }
+
+    fn single_int_parsing_test(i: i64) {
+        let v: Vec<u8> = Encoding::Int(i).iter().collect();
+        let value = Encoding::parse(&v).unwrap_int();
+        assert_eq!(value, i);
+    }
+
+    #[test]
+    fn int_enc_parsing() {
+        for i in std::i16::MIN as i64..std::i16::MAX as i64 + 1 {
+            single_int_parsing_test(i);
+        }
+        for i in (1 << 23) as i64 - 10000..(1 << 23) as i64 + 10000 {
+            single_int_parsing_test(i);
+            single_int_parsing_test(-i);
+        }
+        for i in std::i32::MAX as i64 - 10000..std::i32::MAX as i64 + 10000 {
+            single_int_parsing_test(i);
+            single_int_parsing_test(-i);
+        }
+        for i in std::i64::MAX - 10000..=std::i64::MAX {
+            single_int_parsing_test(i);
+            single_int_parsing_test(-i);
+        }
+        single_int_parsing_test(std::i64::MIN);
+    }
+
+    fn single_str_parsing_test(i: usize) {
+        let v: Vec<u8> = Encoding::Str(i).iter().collect();
+        let value = Encoding::parse(&v).unwrap_str();
+        assert_eq!(value, i);
+    }
+
+    #[test]
+    fn str_enc_parsing() {
+        for i in 0..50000 {
+            single_str_parsing_test(i);
+        }
+        for i in (1 << 32) - 50000..(1 << 32) {
+            single_str_parsing_test(i);
+        }
     }
 }
