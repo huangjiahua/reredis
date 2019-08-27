34aca6421f40b31ce9d0413fc66e765b8f8b5e4d
huangjiahua <hjh4477@outlook.com>
2019-08-27 17:08:06 +0800

    Merge branch 'master' of /home/jiahua/CLionProjects/reredis with conflicts.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,11 @@
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+extern crate jemallocator;
+extern crate jemalloc_sys;
+
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 #[macro_use]
 pub mod shared;
@@ -17,4 +22,4 @@ pub mod protocol;
 pub mod command;
 pub mod util;
 pub mod glob;
-pub mod zalloc;
+pub mod alloc;
